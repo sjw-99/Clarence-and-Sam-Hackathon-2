7c5eff25bab7e33359682889e1eaeefb22b8b5f5
sjw-99 <samwood0312@gmail.com>
2025-12-12 12:18:32 +0000

    Setup db and achieved full functionality

--- server/db/posts.sql
+++ server/db/posts.sql
@@ -10,7 +10,7 @@ CREATE TABLE posts (
     club VARCHAR(100) NOT NULL,
     title VARCHAR(255) NOT NULL,
     text VARCHAR,
-    keywords VARCHAR[] (255),
+    keywords TEXT[],
     PRIMARY KEY (post_id)
 );
 
@@ -19,6 +19,6 @@ INSERT INTO posts (date, time, category, club,  title, text, keywords)
 VALUES
     ('2025-12-12', '09:15:00', 'Injury News', 'Arsenal', 'Gabriel Jesus makes long-awaited return from injury', 'Arsenal fans were delighted to see Gabriel Jesus enter the fray during their 3-0 triumph over Club Brugge on wednesday evening.', '{Jesus, Arsenal, Injury}'),
     ('2025-12-12', '09:30:00', 'Match Report', 'Chelsea', 'Chelsea lose again', 'It was unsurprising to see Chelsea beaten once again, decidedly second-best in their 2-1 defeat to Atalanta', '{Chelsea, Atalanta, Champions League}'),
-    ('2025-12-12', '09:45:00', 'Transfer News', 'Manchester United', "Manchester United plotting swoop for Arsenal's Ethan Nwaneri", 'Manchester United are reportedly preparing a bid for Arsenal academy graduate Ethan Nwaneri when the transfer window opens, who do they think they are?', '{Manchester United, Arsenal, Nwaneri}'),
+    ('2025-12-12', '09:45:00', 'Transfer News', 'Manchester United', 'Manchester United plotting swoop for Arsenal''s Ethan Nwaneri', 'Manchester United are reportedly preparing a bid for Arsenal academy graduate Ethan Nwaneri when the transfer window opens, who do they think they are?', '{Manchester United, Arsenal, Nwaneri}'),
     ('2025-12-12', '10:00:00', 'Club News', 'Arsenal', 'Arsenal close to securing new sleeve sponsor', 'Arsenal are reported to have signed a deal with HR platform Deel to replace their current, controversial, sleeve sponsor Visit Rwanda.', '{Arsenal, Sponsorship, Controversy}');
     
